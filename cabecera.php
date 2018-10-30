25980847392ef62dbcdc15a337cf7e483ec4357d
DiegoL1906 <44480705+DiegoL1906@users.noreply.github.com>
2018-10-30 16:44:55 -0500

    Add files via upload

---
+++ cabecera.php
@@ -0,0 +1,7 @@
+<header>
+    <p id="logo"><img src="https://s3-us-west-2.amazonaws.com/meet-medic-content/page/assets/img/logo.png" height="250px" width="360px"></p>
+    <div id="nom_pag">
+        <p id="titulo">Mediayuda</p>
+        <p id="lema">Tu portal de salud</p>
+    </div>
+</header>
---
+++ navegador.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="presentasion.php">¿Quienes somos?</a></li>
+        <li><a href="metodo.php">¿Cómo trabajamos?</a></li>
+        <li><a href="consulta.php">Realiza una consulta</a></li>
+        <li><a href="registro.php">Únete a nosotros</a></li>
+    </ul>
+</nav>
